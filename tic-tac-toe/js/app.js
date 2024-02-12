42a7e38f1f84062c8c439cfddb9dca7b75898fda
221186 <jdyjsh77@naver.com>
2024-02-12 23:08:40 +0900

    사용자가 이름을 작성하는 js 기능 추가

--- tic-tac-toe/js/app.js
+++ tic-tac-toe/js/app.js
@@ -7,7 +7,15 @@ const DeleteBackdrop = document.getElementById('backdrop');
 const EditOneBtn = document.getElementById('edit-player-1-btn');
 const EdittwoBtn = document.getElementById('edit-player-2-btn');
 
+const Userform = document.querySelector('form');
+const UserInput = document.getElementById('playname');
+const InputError = document.getElementById('config-error');
+
 EditOneBtn.addEventListener('click', OpenPlayerConfig);
 EdittwoBtn.addEventListener('click', OpenPlayerConfig);
+
 DeleteBtn.addEventListener('click', DeletePlayerConfig);
-DeleteBackdrop.addEventListener('click', DeletePlayerConfig)
+DeleteBackdrop.addEventListener('click', DeletePlayerConfig);
+
+Userform.addEventListener('submit', SavePlayerConfig);
+
--- tic-tac-toe/js/config.js
+++ tic-tac-toe/js/config.js
@@ -6,4 +6,17 @@ function OpenPlayerConfig() {
 function DeletePlayerConfig() {
   PlayOverlay.style.display = 'none';
   OpenBackdrop.style.display = 'none';
+  Userform.firstElementChild.classList.remove('error');
+  InputError.textContent = '';
 }
+
+function SavePlayerConfig(event) {
+  event.preventDefault();
+  const formUser = new FormData(event.target);
+  const enteredPlayername = formUser.get('username').trim();
+  
+  if (!enteredPlayername) {
+    event.target.firstElementChild.classList.add('error');
+    InputError.textContent = '유효한 이름을 작성하세요.';
+  }
+ }
